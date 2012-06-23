5f42abeba24268f178e62dccc062ceb2c1f2458c
huangkangjing <huangkangjing@gmail.com>
2012-06-23 23:27:53 +0800

    Moved one-click mobai project files to branch oneclick.

--- js/pop.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    var murl=new RegExp("^http:\/\/www.renren.com\/\d*");
-    chrome.tabs.getSelected(null,function(tab){
-	if(!murl.test(tab.url))
-	{$("#aclick").replaceWith($("<p style='color:red'>Please use this extension at Renren.com!</p>"));
-	}
-    });
-});
