af3b1ea21503922ec86ac0054695d68b38deba44
huangkangjing <huangkangjing@gmail.com>
2012-06-23 22:04:06 +0800

    Completed option page modification.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+//Setting objects for global use.
 var imgurl=chrome.extension.getURL("bai.gif");
-var txt=""
+var txt="";
 for (i=1;i<=10;i++)
 {txt+="(mb)";}
+//Creating Mobai button for items in feeds.
 var $img=$("<img/>");
 $img.attr("src",imgurl).appendTo(".legend")
     .click(function (){
@@ -9,7 +11,7 @@ $img.attr("src",imgurl).appendTo(".legend")
 	e.initEvent('click',true,true);
 	var obj=$(this).parent().siblings(".feed-comment").find("textarea")
 	    .trigger('mouseover').trigger('click').trigger('focus')
-	    .val("Testing Chrome extension, sorry for the inconvenience.")
+	    .val(txt);
 	setTimeout(function (){
 	    obj.next().find("a.submit").get(0)
 	    .dispatchEvent(e);
