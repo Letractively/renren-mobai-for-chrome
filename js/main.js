c2f415b766e1766c17ec6bf9fcf08ef08f9914b3
huangkangjing <huangkangjing@gmail.com>
2012-06-24 09:30:30 +0800

    Set content scripts to activate after DOM is loaded.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
-//Setting objects for global use.
-var imgurl=chrome.extension.getURL("bai.gif");
-var txt="";
-for (i=1;i<=10;i++)
-{txt+="(mb)";}
-//Creating Mobai button for items in feeds.
-var $img=$("<img/>");
-$img.attr("src",imgurl).appendTo(".legend")
-    .click(function (){
-	var e=document.createEvent('MouseEvents');
-	e.initEvent('click',true,true);
-	var obj=$(this).parent().siblings(".feed-comment").find("textarea")
-	    .trigger('mouseover').trigger('click').trigger('focus')
-	    .val(txt);
-	setTimeout(function (){
-	    obj.next().find("a.submit").get(0)
-	    .dispatchEvent(e);
-	},100);
-    });
+$(function(){
+    //Setting objects for global use.
+    var imgurl=chrome.extension.getURL("bai.gif");
+    var txt="";
+    for (i=1;i<=10;i++)
+    {txt+="(mb)";}
+    //Creating Mobai button for items in feeds.
+    var $img=$("<img/>");
+    $img.attr("src",imgurl).appendTo(".legend")
+	.click(function (){
+	    var e=document.createEvent('MouseEvents');
+	    e.initEvent('click',true,true);
+	    var obj=$(this).parent().siblings(".feed-comment").find("textarea")
+		.trigger('mouseover').trigger('click').trigger('focus')
+		.val(txt);
+	    setTimeout(function (){
+		obj.next().find("a.submit").get(0)
+		    .dispatchEvent(e);
+	    },100);
+	});
+});
--- js/share.js
+++ js/share.js
@@ -1,14 +1,16 @@
-var imgurl=chrome.extension.getURL("bai.gif");
-var $inp=$("#miniEditorTextarea");
-var $sub=$("#submit_comment");
-var addtxt="";
-for (i=1;i<=10;i++)
-{
-    addtxt+="(mb)";
-}
-var $img=$("<img />");
-$img.attr("src",imgurl).appendTo("#miniEditorAction")
-    .click(function (){
-	$inp.val($inp.val()+addtxt);
-	$sub.trigger('click');
-    });
+$(function (){
+    var imgurl=chrome.extension.getURL("bai.gif");
+    var $inp=$("#miniEditorTextarea");
+    var $sub=$("#submit_comment");
+    var addtxt="";
+    for (i=1;i<=10;i++)
+    {
+	addtxt+="(mb)";
+    }
+    var $img=$("<img />");
+    $img.attr("src",imgurl).appendTo("#miniEditorAction")
+	.click(function (){
+	    $inp.val($inp.val()+addtxt);
+	    $sub.trigger('click');
+	});
+});
